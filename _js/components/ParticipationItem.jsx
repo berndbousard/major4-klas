6989956b49d5fd5099c1b43b27380f51504a155b
berndbousard <bernd.bousard@student.howest.be>
2016-03-20 21:00:45 +0100

    lijntje bij participations

--- _js/components/ParticipationItem.jsx
+++ _js/components/ParticipationItem.jsx
@@ -8,7 +8,7 @@ export default (props) => {
     <tr className="cms-orders-table-data-row">
       <td className="cms-orders-table-data-cell">{props.name}</td>
       <td className="cms-orders-table-data-cell">{props.school} - {props.class}</td>
-      <td className="cms-orders-table-data-cell cms-orders-table-data-cell-img"><img src={`${basename}/uploads/photo/${props.photo}`} /></td>
+      <td className="cms-orders-table-data-cell"><img className="cms-orders-table-data-cell-img" src={`${basename}/uploads/photo/${props.photo}`} /></td>
       <td className="cms-orders-table-data-cell">{props.email}</td>
       <td className="cms-orders-table-data-cell">{props.created}</td>
     </tr>
