66ab2e37e1c6ff37e47abc69d94a1f01fd532d55
berndbousard <bernd.bousard@student.howest.be>
2016-03-23 21:49:26 +0100

    breakpoints seperated to prevent conflicts

--- view/home.php
+++ view/home.php
@@ -28,11 +28,11 @@
     <nav class="main-nav">
       <ul class="main-nav-list">
         <li class="nav-logo"><a href="http://www.boek.be"><span class="hide">boek.be</span></a></li>
-        <li class="nav-items"> <!-- Hier moet wrapper rond voor responsive -->
+        <div class="nav-items"> <!-- Hier moet wrapper rond voor responsive -->
           <li class="nav-item"><a href="index.html" class="active"><span>de actie</span></a></li>
           <li class="nav-item"><a href="over.html"><span>over de shining</span></a></li>
           <li class="nav-item"><a href="index.html#form-deelnemen"><span>deelnemen</span></a></li>
-        </li>
+        </div>
       </ul>
     </nav>
     <main class="main-wrapper actie">
